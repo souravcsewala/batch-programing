7a880ad9496b7ac750415cb4b026eb9175d4e6f9
souravcsewala <souravssc077@gmail.com>
2025-02-08 11:08:58 +0530

    update

--- remove_space.bat
+++ remove_space.bat
@@ -7,7 +7,7 @@ rem Remove the exact substring "oslh"
 set "a=%a: =%"  &:: remove space 
 set "a=%a:o=%"  &:: remove o from the string 
 set "a=%a:s=%"  &:: remove s from the string 
-set "a=%a:h=%"   &:: remove h from the string 
+set "a=%a:h=%"  &:: remove h from the string 
 echo %a%
 
 pause
