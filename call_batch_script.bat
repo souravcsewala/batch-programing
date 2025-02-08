fbc3d1ec62d72a984c3db92c48ea06bb149989a4
souravcsewala <souravssc077@gmail.com>
2025-02-08 11:21:49 +0530

    update

--- call_batch_script.bat
+++ call_batch_script.bat
@@ -1,5 +1,5 @@
 :: call another batch file from another file 
-@echo off 
+@echo off        
 Title run a batch file from another batch file 
 echo running the second file.... 
 call "E:\batch programing\call.bat"  &:: use the call command to call the another batch script
