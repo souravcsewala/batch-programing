98e9b4bcbb7dd0c1759d8b094d8fcd08b36b6a88
souravcsewala <souravssc077@gmail.com>
2025-02-08 11:08:17 +0530

    update

---
+++ remove_space.bat
@@ -0,0 +1,13 @@
+@echo off
+Title Remove Substring
+
+set "a=hello sourav chiroslhree"
+
+rem Remove the exact substring "oslh"
+set "a=%a: =%"  &:: remove space 
+set "a=%a:o=%"  &:: remove o from the string 
+set "a=%a:s=%"  &:: remove s from the string 
+set "a=%a:h=%"   &:: remove h from the string 
+echo %a%
+
+pause
